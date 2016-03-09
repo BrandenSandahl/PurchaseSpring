4fa169fa8c2031c096810a75b1df72ae0cbeb8eb
BrandenSandahl <branden157@gmail.com>
2016-03-09 16:41:59 -0500

    Table properly displays

--- src/main/java/com/theironyard/PurchaseRepository.java
+++ src/main/java/com/theironyard/PurchaseRepository.java
@@ -2,11 +2,13 @@ package com.theironyard;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by branden on 3/9/16 at 12:55.
  */
 public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
-
+    List<Purchase> findByCustomer(Customer customer);
 
 
 }
--- src/main/java/com/theironyard/PurchasesController.java
+++ src/main/java/com/theironyard/PurchasesController.java
@@ -2,10 +2,14 @@ package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -48,8 +52,14 @@ public class PurchasesController {
             }
             s.close();
         }
+    }
 
 
+    @RequestMapping(name = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+
+        model.addAttribute("purchases", purchaseRepository.findAll());
+        return "home";
     }
 
 }
